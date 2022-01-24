01b28dc1e81af5be47392cedf4e1a289646a448b
Drillu <maxmax98765@yahoo.ro>
2022-01-24 22:07:08 +0200

    building placement

---
+++ The Outpost/Assets/Scripts/buildingSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class buildingSpawner : MonoBehaviour
+{
+    public Tilemap tm;
+    public TileBase one, three;
+    public GameObject buildingOne, buildingThree;
+    public Transform parentOne, parentThree;
+
+    void Start()
+    {
+        foreach(var tile in tm.cellBounds.allPositionsWithin)
+        {
+            if(tm.GetTile(tile) == one)
+            {
+                Instantiate(buildingOne, tm.GetCellCenterLocal(tile), Quaternion.identity,parentOne);
+            }
+            else if (tm.GetTile(tile) == three)
+            {
+                Instantiate(buildingThree, tm.GetCellCenterLocal(tile), Quaternion.identity,parentThree);
+            }
+        }
+    }
+
+    
+}
