331fc01bc95c3e8df9c38c363b3cc5cd86441b7f
Drillu <maxmax98765@yahoo.ro>
2022-01-27 22:14:46 +0200

    ceva buton

--- The Outpost/Assets/Scripts/CanvasSlide.cs
+++ The Outpost/Assets/Scripts/CanvasSlide.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CanvasSlide : MonoBehaviour
 {
     public Animator slideAnimator;
-    public GameObject upButton, downButton;
+    public GameObject upButton, downButton; // TOP
 
+    private float seconds, minutes;
+    private TMP_Text timer; //TOP_RIGHT
+
+    #region Unity Functions
+
+    private void Start()
+    {
+        timer = GameObject.Find("Clock").GetComponent<TMP_Text>();
+        timer.text = "_"; //CLOCk
+    }
+
+    private void Update()
+    {
+        ChangeTime();
+    }
+
+    #endregion
+
+    #region TOP
     IEnumerator WaitThenSwitchButton(GameObject disable, GameObject enable)
     {
         yield return new WaitForSeconds(1f);
@@ -14,17 +34,35 @@ public class CanvasSlide : MonoBehaviour
         enable.SetActive(true);
     }
 
+
     public void GoSlideDown()
     {
         slideAnimator.SetBool("goDown", true);
         StartCoroutine(WaitThenSwitchButton(downButton, upButton));
-       
+
     }
 
     public void GoSlideUp()
     {
         slideAnimator.SetBool("goDown", false);
         StartCoroutine(WaitThenSwitchButton(upButton, downButton));
-        
+
     }
+    #endregion
+
+    #region TOP_RIGHT
+
+    public void ChangeTimeScale(float speed)
+    {
+        Time.timeScale = speed;
+    }
+
+    public void ChangeTime()
+    {
+        seconds = (int)(Time.time % 60);
+        minutes = (int)(Time.time / 60);
+        timer.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+
+    #endregion
 }
