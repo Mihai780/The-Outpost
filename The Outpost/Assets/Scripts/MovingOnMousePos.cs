b59fa74c30923c605423cf604560aaf05403a5cc
Drillu <maxmax98765@yahoo.ro>
2022-01-29 23:06:32 +0200

    cinemachine

---
+++ The Outpost/Assets/Scripts/MovingOnMousePos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MovingOnMousePos : MonoBehaviour
+{
+    Transform thisTransform;
+    Vector2 mousePos;
+
+    private void Start()
+    {
+        thisTransform = this.transform;
+    }
+
+   
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(!EventSystem.current.IsPointerOverGameObject())
+            thisTransform.position = mousePos;
+    }
+}
