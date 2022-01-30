e8816220f9e598b1bbe589651beadfa88d7c1414
Drillu <maxmax98765@yahoo.ro>
2022-01-30 22:08:27 +0200

    survivors

---
+++ The Outpost/Assets/ScriptableObjects/SurvivorDataSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="Survivor Data",menuName ="Survivors/New Data")]
+public class SurvivorDataSO : ScriptableObject
+{
+    public int buildingLevel;
+    public int scavangeingLevel;
+    public int soldierLevel;
+    public int engineerLevel;
+    public int leaderLevel;
+}
---
+++ The Outpost/Assets/Scripts/SurvivorScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.EventSystems;
+
+public class SurvivorScript : MonoBehaviour
+{
+    public SurvivorDataSO data;
+    Vector2 mousePos;
+    Vector3Int spawnPos;
+    public Tilemap baseTM;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+    }
+
+    public void OnMouseDrag()
+    {
+        this.gameObject.transform.position = new Vector3(mousePos.x,mousePos.y ,0);
+    }
+
+}
