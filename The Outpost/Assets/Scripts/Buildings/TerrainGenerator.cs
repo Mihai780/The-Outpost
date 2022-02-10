441258ceb7b6aedd9abd47c116821fdb9e38fba8
Drillu <maxmax98765@yahoo.ro>
2022-02-10 16:54:56 +0200

    gen

--- The Outpost/Assets/Scripts/Buildings/TerrainGenerator.cs
+++ The Outpost/Assets/Scripts/Buildings/TerrainGenerator.cs
@@ -4,36 +4,51 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    public int width, height ,k=0;
+    public int repetariPeAxaX, urcariPeAxaY;
     public List<GameObject> prefabs;
-    public Vector2 currentPoint;
-    
+    public Vector2 origin,currentPoint,addTile1,addTile3;
+
 
     void Start()
     {
-        while(k<10)
+        currentPoint = origin;
+        for(int x=1;x<=repetariPeAxaX;x++)
         {
-            int index = Random.Range(0, prefabs.Count);
-            GameObject selectedPrefab = prefabs[index];
-
-            if(index == 0)
+            for(int y=1;y<=urcariPeAxaY;y++)
             {
-                Instantiate(selectedPrefab, currentPoint, Quaternion.identity);
-                currentPoint = currentPoint + new Vector2(1f, 0.5f);
+                //Verifici daca pe pozitia curenta si pe urmatoarea se poate pune un tile
+                    //daca poti pe amandoua dai random
+                        //-tile1 sau 2
+                    //daca poti doar pe tile-ul curent pui tile1 si muti
+                    //daca nu poti pe nicicunul doar muti
 
-            }
-            else if(index==1)
-            {
-                
-                Instantiate(selectedPrefab, currentPoint, Quaternion.identity);
-                currentPoint = currentPoint + new Vector2(2f, 1f);
+                if(!Physics2D.CircleCast(currentPoint + addTile1, 0.1f, currentPoint + addTile1) && !Physics2D.CircleCast(currentPoint, 0.1f, currentPoint))
+                {
+                    int index = Random.Range(0, prefabs.Count);
+                    GameObject selectedPrefab = prefabs[index];
+                    Instantiate(selectedPrefab, currentPoint, Quaternion.identity);
+                    if (index == 0)
+                        currentPoint += addTile1;
+                    else if(index == 1)
+                        currentPoint += addTile3;
 
+                }
+
+                else if(!Physics2D.CircleCast(currentPoint, 0.1f, currentPoint))
+                {
+                    Instantiate(prefabs[0], currentPoint, Quaternion.identity);
+                    currentPoint += addTile1;
+                }
+
+                else
+                {
+                    currentPoint += addTile1;
+                }
             }
-            
-            k++;
+            currentPoint = origin + (x* new Vector2(-1f,0.5f));
         }
+
         
     }
-
     
 }
