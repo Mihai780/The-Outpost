d5ce21a5b0e41b416f2b3afcc97410085784d808
Drillu <maxmax98765@yahoo.ro>
2022-02-13 23:18:00 +0200

    d

---
+++ The Outpost/Assets/Scripts/Buildings/TilemapTerrainGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapTerrainGenerator : MonoBehaviour
+{
+    public Tilemap tm;
+    public TileBase tile1, tile3;
+    public GameObject prefab1, prefab3;
+    
+    void Start()
+    {
+        foreach(var tile in tm.cellBounds.allPositionsWithin)
+        {
+            if(tm.GetTile(tile) == tile1)
+            {
+                Instantiate(prefab1, tm.GetCellCenterWorld(tile), Quaternion.identity);
+            }
+            if(tm.GetTile(tile) == tile3)
+            {
+                Instantiate(prefab3, tile, Quaternion.identity);
+            }
+        }
+    }
+
+   
+}
