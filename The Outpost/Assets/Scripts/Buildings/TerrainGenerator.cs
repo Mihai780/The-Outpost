1d2d6e0b6490aa634dfae89ad40f5aecb0694f38
Drillu <maxmax98765@yahoo.ro>
2022-02-16 21:10:10 +0200

    cd

--- The Outpost/Assets/Scripts/Buildings/TerrainGenerator.cs
+++ The Outpost/Assets/Scripts/Buildings/TerrainGenerator.cs
@@ -8,59 +8,71 @@ public class TerrainGenerator : MonoBehaviour
     public List<GameObject> prefabs;
     public Vector2 origin,currentPoint,addTile1,addTile3;
     public Transform parentTile1, parentTile3;
-
+    private int x,y;
 
     void Start()
     {
-        currentPoint = origin;
-        for(int x=1;x<=repetariPeAxaX;x++)
-        {
-            for(int y=1;y<=urcariPeAxaY;y++)
-            {
-                //Verifici daca pe pozitia curenta si pe urmatoarea se poate pune un tile
-                //daca poti pe amandoua dai random
-                //-tile1 sau 2
-                //daca poti doar pe tile-ul curent pui tile1 si muti
-                //daca nu poti pe nicicunul doar muti
-
-                if (Physics2D.CircleCast(currentPoint, 0.1f, currentPoint) && Physics2D.CircleCast(currentPoint + addTile1, 0.1f, currentPoint + addTile1))
-                {
-                    currentPoint += addTile3;
-                }
-
-                else if (!Physics2D.CircleCast(currentPoint, 0.1f, currentPoint) && Physics2D.CircleCast(currentPoint + addTile1, 0.1f, currentPoint + addTile1))
-                {
-                    Instantiate(prefabs[0], currentPoint, Quaternion.identity,parentTile1);
-                    currentPoint += addTile1;
-                }
-
-                else if (!Physics2D.CircleCast(currentPoint + addTile1, 0.1f, currentPoint + addTile1) && !Physics2D.CircleCast(currentPoint, 0.1f, currentPoint))
-                {
-                    int index = Random.Range(0, prefabs.Count);
-                    GameObject selectedPrefab = prefabs[index];
-                    
-                    if (index == 0)
-                    {
-                        Instantiate(selectedPrefab, currentPoint, Quaternion.identity,parentTile1);
-                        currentPoint += addTile1;
-                    }
-                        
-                    else if(index == 1)
-                    {
-                        Instantiate(selectedPrefab, currentPoint, Quaternion.identity,parentTile3);
-                        currentPoint += addTile3;
-                    }
-                        
-
-                }
-
-                else 
-                {
-                    currentPoint += addTile1;
-                }
-            }
-            currentPoint = origin + (x* new Vector2(-1f,0.5f));
-        }
+        x = 1;
+        y = 1;
+        Invoke(nameof(Generate),0.5f);
     }
     
+    void XCall()
+    {
+        currentPoint = origin + (x * new Vector2(-1f, 0.5f));
+        if (x <= repetariPeAxaX)
+            Invoke(nameof(YCall), 0.1f);
+    }
+
+    void YCall()
+    {
+        if (!Physics2D.CircleCast(currentPoint + addTile1, 0.1f, currentPoint + addTile1) && !Physics2D.CircleCast(currentPoint, 0.1f, currentPoint))
+        {
+            int index = Random.Range(0, prefabs.Count);
+            GameObject selectedPrefab = prefabs[index];
+
+            if (index == 0)
+            {
+                Instantiate(selectedPrefab, currentPoint, Quaternion.identity, parentTile1);
+                currentPoint += addTile1;
+            }
+
+            else if (index == 1)
+            {
+                Instantiate(selectedPrefab, currentPoint, Quaternion.identity, parentTile3);
+                currentPoint += addTile1;
+            }
+
+        }
+
+        else if (!Physics2D.CircleCast(currentPoint, 0.1f, currentPoint) && Physics2D.CircleCast(currentPoint + addTile1, 0.1f, currentPoint + addTile1))
+        {
+            Instantiate(prefabs[0], currentPoint, Quaternion.identity, parentTile1);
+            currentPoint += addTile1;
+        }
+
+        else
+        {
+            currentPoint += addTile1;
+        }
+
+        y++;
+        if (y <= urcariPeAxaY)
+        {
+            Invoke(nameof(YCall), 0.1f);
+        }
+        else
+        {
+            y = 1;
+            Invoke(nameof(XCall), 0.5f);
+            x++;
+        }
+    }
+
+    void Generate()
+    {
+        currentPoint = origin;
+
+        Invoke(nameof(XCall),0.5f);
+    }
 }
